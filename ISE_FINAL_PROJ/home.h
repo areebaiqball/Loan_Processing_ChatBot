9ec947540476bfa121a59fb77d490cfd34d7a560
Maryamkiani09 <maryamkiani067@gmail.com>
2025-10-26 17:42:30 +0500

    Added home.cpp and home.h

---
+++ ISE_FINAL_PROJ/home.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef HOME_H
+#define HOME_H
+
+#include <string>
+using namespace std;
+
+/// <summary>
+/// Represents home loan details
+/// </summary>
+class HomeLoan {
+private:
+    string area;
+    string size;
+    int installments;
+    long long price;
+    long long downPayment;
+
+public:
+    // Constructor 
+    HomeLoan();
+    HomeLoan(const string& a, const string& s, int inst, long long p, long long dp);
+
+    // Getters 
+    string getArea() const;
+    string getSize() const;
+    int getInstallments() const;
+    long long getPrice() const;
+    long long getDownPayment() const;
+
+    // Setters
+    void setArea(const string& a);
+    void setSize(const string& s);
+    void setInstallments(int inst);
+    void setPrice(long long p);
+    void setDownPayment(long long dp);
+};
+
+/// <summary>
+/// Loads home loan data from file
+/// </summary>
+/// <param name="loans">loans Array to store loans</param>
+/// <param name="maxSize">Maximum array size</param>
+/// <param name="filename">File to load from</param>
+/// <returns>Number of loans loaded</returns>
+int loadHomeLoans(HomeLoan loans[], int maxSize, const string& filename);
+
+/// <summary>
+/// Displays home loan options for area
+/// </summary>
+/// <param name="loans">Array of loans</param>
+/// <param name="size">Array size</param>
+/// <param name="areaNumber">Area identifier</param>
+/// <returns>True if data found, false otherwise</returns>
+bool displayHomeLoanOptions(const HomeLoan loans[], int size, const string& areaNumber);
+
+#endif
